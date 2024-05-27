4bfc5c27f466d8168f1c1e2ed619dd36570fd201
huzaifaks123 <112252562+huzaifaks123@users.noreply.github.com>
2024-05-28 00:30:27 +0530

    Update TrendAnalysis.js

--- src/pages/TrendAnalysis.js
+++ src/pages/TrendAnalysis.js
@@ -18,11 +18,11 @@ export default function TrendAnalysis() {
                 <div className={`col-md-8 me-2 p-0 rounded ${styles.HeroCard}`}>
                     <Hero />
                 </div>
-                <div className={`col-md border border-danger rounded p-2 ${styles.card}`}><SplineChart data={data} /></div>
+                <div className={`col-md rounded p-2 ${styles.card}`}><SplineChart data={data} /></div>
             </div>
             <div className={`container row gx-5 d-flex justify-content-between w-100 mx-0 p-2 ${styles.container}`}>
-                <div className={`col-md border mx-1 border-danger rounded p-2 ${styles.card}`}><AreaChart data={data} /></div>
-                <div className={`col-md border mx-1 border-danger rounded p-2 ${styles.card}`}><StepAreaChart data={data} /></div>
+                <div className={`col-md mx-1 rounded p-2 ${styles.card}`}><AreaChart data={data} /></div>
+                <div className={`col-md mx-1 rounded p-2 ${styles.card}`}><StepAreaChart data={data} /></div>
             </div>
         </>
     )
